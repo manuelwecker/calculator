50ef9eb8c9a3fdbc9c58f7fc46a030846882c508
manuelwecker <manuelwecker@web.de>
2019-11-06 13:50:53 +0100

    add functions and operators

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
+"use strict";
+
+import { sum, sub, multi, div } from "./lib/operators";
+import { changecolor } from "./lib/design";
+
 const firstInput = document.querySelector("[name=first]");
 const secondInput = document.querySelector("[name=second]");
-const submitButton = document.querySelector(".submit");
+const sumButton = document.querySelector(".sum");
+const subButton = document.querySelector(".sub");
+const multiButton = document.querySelector(".multi");
+const divButton = document.querySelector(".div");
 
-submitButton.addEventListener("click", function() {
-  const sum = parseInt(firstInput.value) + parseInt(secondInput.value);
-  alert(sum);
+function getInput() {
+  const firstInput = document.querySelector("[name=first]");
+  const secondInput = document.querySelector("[name=second]");
+  return {
+    first: a,
+    second: b
+  };
+}
+
+sumButton.addEventListener("click", function() {
+  const firstInt = parseInt(firstInput.value);
+  const secondInt = parseInt(secondInput.value);
+  const result = document.querySelector("#result");
+  result.innerHTML = sum(firstInt, secondInt);
+  changecolor();
+});
+
+// sumButton.addEventListener("click", function() {
+//   const numbers = getInput();
+//   result.innerHTML = sum(numbers.a, numbers.b);
+// });
+
+subButton.addEventListener("click", function() {
+  const firstInt = parseInt(firstInput.value);
+  const secondInt = parseInt(secondInput.value);
+  const result = document.querySelector("#result");
+  result.innerHTML = sub(firstInt, secondInt);
+  document.querySelector("#result").style.color = "green";
+});
+
+multiButton.addEventListener("click", function() {
+  const firstInt = parseInt(firstInput.value);
+  const secondInt = parseInt(secondInput.value);
+  const result = document.querySelector("#result");
+  result.innerHTML = multi(firstInt, secondInt);
+});
+
+divButton.addEventListener("click", function() {
+  const firstInt = parseInt(firstInput.value);
+  const secondInt = parseInt(secondInput.value);
+  const result = document.querySelector("#result");
+  result.innerHTML = div(firstInt, secondInt);
 });
